c70d3e855569b97fb965e6b254a75d0b6f38dd80
张崇 <nuptunee@gmail.com>
2020-05-29 16:01:25 +0800

    feat:添加pm2运行文件

---
+++ run.js
@@ -0,0 +1,11 @@
+
+// pm2入口文件
+const { exec } = require('child_process')
+exec('hexo server -p 4000 & ', (error, stdout, stderr) => {
+	if (error) {
+		console.log('exec error: ${error}')
+		return
+	}
+	console.log('stdout: ${stdout}');
+	console.log('stderr: ${stderr}');
+})
